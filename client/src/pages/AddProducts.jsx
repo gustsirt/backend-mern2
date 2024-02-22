c95d89e4976e48d6b0a2e5be9780b0f7973a6616
Gustavo - PC Trabajo <gustavo.sirtori@gmail.com>
2024-02-22 15:34:46 -0300

    implementando service

--- client/src/pages/AddProducts.jsx
+++ client/src/pages/AddProducts.jsx
@@ -3,11 +3,13 @@ import { ContextConfig } from "../context/ContextConfig.jsx";
 //import { ContextUser } from "./context/ContextUser.jsx";
 import Swal from 'sweetalert2';
 import ProductsList from "../components/products/ProductsList.jsx";
+import ServiceProducts from "../services/products.jsx";
 
 const AddProducts = () => {
   const { uriBase } = useContext(ContextConfig);
   //const { token } = useContext(ContextUser);
   const [products, setProducts] = useState([]);
+  const serviceProducts = new ServiceProducts()
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -20,17 +22,11 @@ const AddProducts = () => {
           // },
         // };
         //const resp = await fetch(`${uriBase}api/products/`, requestOptions);
-        const resp = await fetch(`${uriBase}api/products/`);
-
-        if (!resp.ok) {
-          throw new Error(`Error en la solicitud: ${resp.status} ${resp.statusText}`);
-        }
-
-        const respJson = await resp.json();
-        //console.log(respJson.payload)
+        const resp = await serviceProducts.get()
+        console.log(resp);
 
         if (respJson?.isError === false) {
-          setProducts(respJson.payload.docs);
+          setProducts(resp.payload.docs);
         } else {
           Swal.fire({ icon: "error", text: respJson.message || "Error al obtener productos" });
         }
@@ -47,12 +43,12 @@ const AddProducts = () => {
   return (
     <div className="page-container">
       <h1 className="title">Editar Productos</h1>
-      <div className="flex-row-50">
-        <div className="row-50">
+      <div className="flex-row">
+        <div className="row-70">
           // TODO FALTA agregar paginacion
           <ProductsList products={products}/>
         </div>
-        <div className="row-50">
+        <div className="row-30">
         </div>
       </div>
     </div>
--- client/src/services/products.jsx
+++ client/src/services/products.jsx
@@ -1,7 +1,68 @@
-const serviceProducts = () => {
-  return (
-    <div>products</div>
-  )
-}
+import { useContext} from "react";
+import { ContextConfig } from "../context/ContextConfig.jsx";
+
+
 // TODO aca poner todos los fetch products
-export default serviceProducts
+export default class ServiceProducts {
+  constructor () {
+    this.uriBase = { uriBase } = useContext(ContextConfig);
+    this.headers = { "Content-Type": "application/json" };
+  }
+
+  get = async () => {
+    const resp = await fetch(`${this.uriBase}api/products/`);
+    console.log(resp);
+    return await resp.json();
+  }
+}
+
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const URI_BASE = process.env.REACT_APP_API_URL;
+
+// class ApiService {
+
+//   constructor() {
+//     this.headers = {
+//       "Content-Type": "application/json",
+//     };
+//   }
+
+//   async get(endpoint) {
+//     const response = await fetch(`${URI_BASE}${endpoint}`, {
+//       method: "GET",
+//       headers: this.headers,
+//     });
+//     return await response.json();
+//   }
+
+//   async post(endpoint, data) {
+//     const response = await fetch(`${URI_BASE}${endpoint}`, {
+//       method: "POST",
+//       headers: this.headers,
+//       body: JSON.stringify(data),
+//     });
+//     return await response.json();
+//   }
+
+//   async put(endpoint, data) {
+//     const response = await fetch(`${URI_BASE}${endpoint}`, {
+//       method: "PUT",
+//       headers: this.headers,
+//       body: JSON.stringify(data),
+//     });
+//     return await response.json();
+//   }
+
+//   async delete(endpoint) {
+//     const response = await fetch(`${URI_BASE}${endpoint}`, {
+//       method: "DELETE",
+//       headers: this.headers,
+//     });
+//     return await response.json();
+//   }
+// }
+
+// export default new ApiService();
